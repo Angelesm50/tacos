531a6a3993ad02defd9eec8a310a568923ab9b56
Angeles MartÃinez <angelesmava0@gmail.com>
2020-09-06 22:01:39 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+(function($){
+  $(function(){
+    // Parallax efect
+    $('.parallax-window-1').parallax({ imageSrc: '../tacos/img/logos/taco-de-birria.png' });
+    $('.parallax-window-2').parallax({ imageSrc: '../tacos/img/tacos-de-birria.jpg' });
+
+    // Back to top scroll efect
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 200) {
+        $(".phone-call").fadeIn(200);
+      } else {
+        $(".phone-call").fadeOut(200);
+      }
+    });
+    $(".phone-call").click(function (event) {
+      event.preventDefault();
+      $("html, body").animate({ scrollTop: 0 }, 300);
+    });
+
+    new WOW().init();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
